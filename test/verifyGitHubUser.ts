a48bf2b09f24256e7f4a608f6c2eec1fc6e461f8
Jessica Kerr <jessitron@gmail.com>
2017-10-17 11:46:55 -0500

    Check that your token will work before testing

---
+++ test/verifyGitHubUser.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { GitHubBase } from "../src/project/git/GitProject";
+import { GitHubToken } from "./atomist.config";
+import { exit } from "shelljs";
+
+const GitHubUserWhoCanRunTheTests = "atomist-travisorg";
+
+const config = {
+    headers: {
+        Authorization: `token ${GitHubToken}`,
+    },
+};
+
+axios.get(`${GitHubBase}/user`, config)
+    .then(response => {
+        if (response.data.login !== GitHubUserWhoCanRunTheTests) {
+            console.error("To run these tests, set GITHUB_TOKEN to a token for atomist-travisorg");
+            process.exit(1);
+        }
+    })
+    .catch(error => {
+        console.error("Could not connect to GitHub: " + error.message);
+        process.exit(1);
+    })
+    ;
