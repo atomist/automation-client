9413234a81daf8b17fa35c9b0758268031a8056b
Jessica Kerr <jessitron@gmail.com>
2017-10-17 15:17:43 -0500

    The client can't tell us everything

--- src/internal/transport/websocket/WebSocketClient.ts
+++ src/internal/transport/websocket/WebSocketClient.ts
@@ -145,7 +145,11 @@ function register(registrationCallback: () => any, options: WebSocketClientOptio
                     && (error.response.status === 403)) {
                     const furtherInfo = error.response.data ? `\nFurther information: ${error.response.data}` : "";
                     logger.error(
-                        `Authentication failed for ${nameVersion} in teams ${registrationPayload.team_ids}.` +
+                        `Authentication failed for ${nameVersion} in teams ${registrationPayload.team_ids}.
+This could be caused by:
+- The Atomist app has not been authorized in the Slack team
+- You are not a member of each GitHub organization associated with the Slack team
+- Your GitHub token doesn't provide org:read permissions` +
                         furtherInfo);
                     process.exit(1);
                 } else {
