6d0d4027859c1cdd1f7e4302694908234073f8a6
Jessica Kerr <jessitron@gmail.com>
2017-10-17 19:19:37 -0500

    Make PlainHelloWorld run from the dashboard

--- test/command/PlainHelloWorld.ts
+++ test/command/PlainHelloWorld.ts
@@ -13,7 +13,8 @@ export class PlainHelloWorld implements HandleCommand, CommandHandlerMetadata {
 
     public description = "Sends a hello back to the client";
     public intent = [ "hello world" ];
-    public parameters = [ { name: "name", pattern: "^.*$", required: true } ];
+    public parameters = [ { name: "name", display_name: "Name", pattern: "^.*$", required: true } ];
+    public mapped_parameters = [];
     public secrets = [ { name: "userToken", path: Secrets.UserToken } ];
 
     public name: string;
