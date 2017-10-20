b3f335851b0bfee73f306653cc8796039b94b991
Jessica Kerr <jessitron@gmail.com>
2017-10-20 17:13:48 -0500

    move try/catch into toAction

--- src/action/actionOps.ts
+++ src/action/actionOps.ts
@@ -17,42 +17,35 @@ export function actionChain<T>(...steps: Array<Chainable<T>>): TAction<T> {
         steps.reduce((c1, c2) => {
             const ed1: TAction<T> = toAction(c1);
             const ed2: TAction<T> = toAction(c2);
-            return p => {
-                let firstResult;
-                try {
-                    firstResult = ed1(p)
-                } catch (error) {
-                    return Promise.resolve(failureOn(p, error, c1))
-                }
-
-                return ed1(p).then(r1 => {
-                    console.log("Applied action " + c1.toString());
-                    let secondResult;
-                    try {
-                        secondResult = ed2(r1.target);
-                    } catch (err) {
-                        return Promise.resolve(failureOn(p, err, c2))
-                    }
-                    return secondResult.then(r2 => {
+            return p => ed1(p).then(r1 => {
+                // console.log("Applied action " + c1.toString());
+                if (!r1.success) { return r1 } else {
+                    return ed2(r1.target).then(r2 => {
                         // console.log("Applied action " + c2.toString());
                         return {
                             ...r1, // the clojure ppl will LOVE this (I love it)
                             ...r2,
-                        };
-                    });;
-                })
-            }
+                        }
+                    })
+                }
+            });
         }) as TAction<T>;
 }
 
 function toAction<T>(link: Chainable<T>): TAction<T> {
-    return t => (link as TOp<T>)(t) // how about a nice catch with a failure ??
-        .then(r => {
-            // See what it returns
-            return isActionResult(r) ?
-                r :
-                successOn(r);
-        });
+    return t => {
+        try {
+            return (link as TOp<T>)(t).then(r => {
+                // See what it returns
+                return isActionResult(r) ?
+                    r :
+                    successOn(r);
+            });
+        }
+        catch (error) {
+            return Promise.resolve(failureOn(t, error, link))
+        }
+    }
 }
 
 /**
