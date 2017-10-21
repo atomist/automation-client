e5b5d4072c2bfc4a66cfad7651757ffc706bf7ad
Jessica Kerr <jessitron@gmail.com>
2017-10-21 11:59:48 -0700

    simpler

--- src/operations/support/editorUtils.ts
+++ src/operations/support/editorUtils.ts
@@ -27,27 +27,23 @@ export function editRepo<T extends Parameters>(
     editor: ProjectEditor,
     ei: EditMode,
     parameters?: T): Promise<EditResult> {
-    // why bother making a promise?
-    const loadRepo: Promise<Project> = Promise.resolve(repo);
     if (isPullRequest(ei)) {
-        return loadRepo.then(gp =>
-            editProjectUsingPullRequest(context, gp as GitProject, editor, ei, parameters));
+        return editProjectUsingPullRequest(context, repo as GitProject, editor, ei, parameters);
     } else if (isBranchCommit(ei)) {
-        return loadRepo.then(gp =>
-            editProjectUsingBranch(context, gp as GitProject, editor, ei, parameters));
+        return editProjectUsingBranch(context, repo as GitProject, editor, ei, parameters));
     } else if (isCustomExecutionEditMode(ei)) {
-        return loadRepo.then(ei.edit);
+        return ei.edit(repo);
     } else {
         // No edit to do
-        return loadRepo.then(gp => successfulEdit(gp, true));
+        return Promise.resolve(successfulEdit(repo, true));
     }
 }
 
 export function editProjectUsingPullRequest<T extends Parameters>(context: HandlerContext,
-                                                                  gp: GitProject,
-                                                                  editor: ProjectEditor<T>,
-                                                                  pr: PullRequest,
-                                                                  parameters?: T): Promise<EditResult> {
+    gp: GitProject,
+    editor: ProjectEditor<T>,
+    pr: PullRequest,
+    parameters?: T): Promise<EditResult> {
 
     return editor(gp, context, parameters)
         .then(r => r.edited ?
@@ -60,10 +56,10 @@ export function editProjectUsingPullRequest<T extends Parameters>(context: Handl
 }
 
 export function editProjectUsingBranch<T extends Parameters>(context: HandlerContext,
-                                                             gp: GitProject,
-                                                             editor: ProjectEditor<T>,
-                                                             ci: BranchCommit,
-                                                             parameters?: T): Promise<EditResult> {
+    gp: GitProject,
+    editor: ProjectEditor<T>,
+    ci: BranchCommit,
+    parameters?: T): Promise<EditResult> {
 
     return editor(gp, context, parameters)
         .then(r => r.edited ?
