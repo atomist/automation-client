b3552d6361adf850e65c8425128a340a49ed9a4f
Christian Dupuis <cd@atomist.com>
2017-10-23 02:46:11 +0200

    fix copyright

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2017  Atomist
+ * Copyright © 2017 Atomist
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
