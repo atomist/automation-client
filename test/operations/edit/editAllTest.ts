fbe037043a6df30d39b3c1494c6b69399f52b6a9
Jessica Kerr <jessitron@gmail.com>
2017-11-07 14:30:16 -0600

    add a test for failed edit result
    And make the custom edit mode able to call the editor. Because before it was skipping that step,
    it wasn't even calling the editor at all. Which made it more than custom persistence, it was
    also being the editor

--- test/operations/edit/editAllTest.ts
+++ test/operations/edit/editAllTest.ts
@@ -6,7 +6,7 @@ import { fromListRepoFinder, fromListRepoLoader } from "../../../src/operations/
 import { GitHubRepoRef } from "../../../src/operations/common/GitHubRepoRef";
 import { editAll } from "../../../src/operations/edit/editAll";
 import { CustomExecutionEditMode } from "../../../src/operations/edit/editModes";
-import { ProjectEditor, successfulEdit } from "../../../src/operations/edit/projectEditor";
+import { failedEdit, ProjectEditor, successfulEdit } from "../../../src/operations/edit/projectEditor";
 import { InMemoryProject } from "../../../src/project/mem/InMemoryProject";
 import { Project } from "../../../src/project/Project";
 
@@ -14,7 +14,7 @@ describe("editAll", () => {
 
     it("should edit repo", done => {
         class Params {
-            public name: string;
+            constructor(public name: string) {}
         }
 
         const editor: ProjectEditor<Params> = (p, ctx, params) => {
@@ -31,20 +31,55 @@ describe("editAll", () => {
 
         const cei: CustomExecutionEditMode = {
             message: "Thing",
-            edit: p => {
+            edit: (p, theEditor, ctx, params) => {
                 projectsEdited.push(p);
-                return Promise.resolve(successfulEdit(p));
+                return theEditor(p, ctx, params);
             },
         };
 
         editAll(null, null, editor, cei,
-            new Params(),
+            new Params("antechinus"),
             fromListRepoFinder(projects),
             p => true,
             fromListRepoLoader(projects))
             .then(edits => {
                 assert(edits.length === projects.length);
                 assert(!edits.some(e => !e.edited));
+                assert.deepEqual(projectsEdited, projects);
+                done();
+            }).catch(done);
+
+    });
+
+    it("should edit repo", done => {
+
+        const editor: ProjectEditor<{}> = (p, ctx, params) => {
+            return Promise.resolve(failedEdit(p, new Error("this didn't work")));
+        };
+
+        const projects = [
+            new InMemoryProject(new GitHubRepoRef("org", "name")),
+        ];
+
+        const projectsEdited: Project[] = [];
+
+        const cei: CustomExecutionEditMode = {
+            message: "Thing",
+            edit: (p, theEditor, ctx, params) => {
+                projectsEdited.push(p);
+                return theEditor(p, ctx, params);
+            },
+        };
+
+        editAll(null, null, editor, cei,
+            {},
+            fromListRepoFinder(projects),
+            p => true,
+            fromListRepoLoader(projects))
+            .then(edits => {
+                assert(edits.length === projects.length);
+                assert(edits.some(e => !e.edited), JSON.stringify(edits));
+                assert(edits.some(e => !e.success));
                 assert.deepEqual(projectsEdited, projects);
                 done();
             }).catch(done);
