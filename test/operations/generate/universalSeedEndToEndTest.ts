8cca2ec30336916c2835fb028092984834b340d5
Rod Johnson <rod@atomist.com>
2017-11-13 14:52:46 +1100

    Fixes #79: Retain permissions from GitHub seed projects

--- test/operations/generate/universalSeedEndToEndTest.ts
+++ test/operations/generate/universalSeedEndToEndTest.ts
@@ -13,6 +13,12 @@ import { UniversalSeed } from "../../../src/operations/generate/UniversalSeed";
 import { NodeFsLocalProject } from "../../../src/project/local/NodeFsLocalProject";
 import { hasFile } from "../../../src/util/gitHub";
 import { GitHubToken } from "../../atomist.config";
+import { Project } from "../../../src/project/Project";
+import { GitProject } from "../../../src/project/git/GitProject";
+import { GitCommandGitProject } from "../../../src/project/git/GitCommandGitProject";
+import { LocalProject } from "../../../src/project/local/LocalProject";
+import * as fs from "fs";
+import { fail } from "power-assert";
 
 const TargetRepo = `test-repo-${new Date().getTime()}`;
 let TargetOwner = "johnsonr";
@@ -61,10 +67,29 @@ describe("Universal seed end to end", () => {
                 return hasFile(GitHubToken, TargetOwner, TargetRepo, "pom.xml")
                     .then(r => {
                         assert(r);
-                        done();
+                        GitCommandGitProject.cloned({token: GitHubToken},
+                            new GitHubRepoRef(TargetOwner, TargetRepo))
+                            .then(verifyPermissions)
+                            .then(() => done());
                     });
             }).catch(done);
     }).timeout(20000);
+
+    function verifyPermissions(p: LocalProject): Promise<Project> {
+        // Check that Maven wrapper mvnw from Spring project is executable
+        const path = p.baseDir + "/mvnw";
+        assert(fs.statSync(path).isFile());
+        // const mode = fs.statSync(path).mode;
+        // if (isEx)
+        fs.access(path, fs.constants.X_OK, err => {
+            if (err) {
+                fail("Not writable");
+            } else {
+                console.log("Valid mode " + fs.statSync(path).mode);
+            }
+        });
+        return Promise.resolve(p);
+    }
 
 });
 
