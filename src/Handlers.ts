4389d8e408ac671654c0a25ae956b2101c9db3a0
Rod Johnson <rod@atomist.com>
2017-11-14 14:19:46 +1100

    Reintroduced handlers.ts for backward compatibility

---
+++ src/Handlers.ts
@@ -0,0 +1,48 @@
+// Deprecated
+// Included only for backward compatibility. Import from root.
+
+import {
+    CommandHandler,
+    EventHandler,
+    MappedParameter, MappedParameters,
+    Parameter,
+    Secret,
+    Tags,
+} from "./decorators";
+import { HandleCommand } from "./HandleCommand";
+import {
+    EventFired,
+    HandleEvent,
+} from "./HandleEvent";
+import { HandlerContext } from "./HandlerContext";
+import {
+    Failure,
+    failure,
+    HandlerResult,
+    RedirectResult,
+    success,
+    Success,
+} from "./HandlerResult";
+
+export {
+    HandlerResult,
+    HandlerContext,
+    HandleCommand,
+    HandleEvent,
+    EventFired,
+    Success,
+    success,
+    Failure,
+    failure,
+    RedirectResult,
+};
+export {
+    EventHandler,
+    Parameter,
+    CommandHandler,
+    MappedParameter,
+    Secret,
+    Tags,
+};
+
+export { MappedParameters, Secrets } from "./decorators";
