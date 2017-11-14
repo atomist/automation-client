a62c4eede086a4d7100c9fec847f78b405395360
Rod Johnson <rod@atomist.com>
2017-11-14 21:13:55 +1100

    Better test for TypeScript type removal via path expressions

--- test/tree/ast/typescript/conversionTests.ts
+++ test/tree/ast/typescript/conversionTests.ts
@@ -1,10 +1,23 @@
 import "mocha";
 
+import { UnionPathExpression } from "@atomist/tree-path/path/pathExpression";
+import { parsePathExpression } from "@atomist/tree-path/path/pathExpressionParser";
 import * as assert from "power-assert";
 import { InMemoryFile } from "../../../../src/project/mem/InMemoryFile";
 import { InMemoryProject } from "../../../../src/project/mem/InMemoryProject";
 import { findMatches } from "../../../../src/tree/ast/astUtils";
 import { TypeScriptES6FileParser } from "../../../../src/tree/ast/typescript/TypeScriptFileParser";
+
+import { CFamilyLangHelper } from "@atomist/microgrammar/matchers/lang/cfamily/CFamilyLangHelper";
+
+const allTypeMatches = [
+    "//VariableDeclaration//ColonToken/following-sibling::*",
+    "//VariableDeclaration//ColonToken",
+];
+
+const allWhiteSpace: UnionPathExpression = {
+    unions: allTypeMatches.map(pe => parsePathExpression(pe)),
+};
 
 /**
  * Experiments in large scale editing: Removing type annotations etc
@@ -28,13 +41,17 @@ describe("path expression driven conversion", () => {
             }).catch(done);
     });
 
-    it("removes type annotation", done => {
-        const f = new InMemoryFile("src/test.ts", "const s: string = '64';");
+    it("removes type annotation on variable declaration", done =>
+        removesTypeAnnotationsIn("const s: string = '64';",
+            "const s  = '64';", done));
+
+    function removesTypeAnnotationsIn(src: string, after: string, done) {
+        const f = new InMemoryFile("src/test.ts", src);
         const p = InMemoryProject.of(f);
 
         findMatches(p, TypeScriptES6FileParser,
             "src/**/*.ts",
-            "//VariableDeclaration//ColonToken/following-sibling::* | //VariableDeclaration//ColonToken")
+            allWhiteSpace)
             .then(values => {
                 assert(values.length === 2);
                 console.log(`Value is [${values[0].$value}]`);
@@ -44,9 +61,11 @@ describe("path expression driven conversion", () => {
             })
             .then(() => {
                 const f2 = p.findFileSync(f.path);
-                assert.equal(f2.getContentSync(), "const s  = '64';");
+                const h = new CFamilyLangHelper();
+                assert.equal(h.canonicalize(f2.getContentSync()),
+                    h.canonicalize(after));
                 done();
             }).catch(done);
-    });
+    }
 
 });
