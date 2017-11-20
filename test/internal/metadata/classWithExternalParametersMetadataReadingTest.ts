9f8c0d87c9b532723e02766ade6b50592e2ecad3
Rod Johnson <rod@atomist.com>
2017-11-20 18:56:04 +1100

    More tests for parameter nesting

--- test/internal/metadata/classWithExternalParametersMetadataReadingTest.ts
+++ test/internal/metadata/classWithExternalParametersMetadataReadingTest.ts
@@ -7,6 +7,7 @@ import { HandleCommand } from "../../../src/HandleCommand";
 import { HandlerContext } from "../../../src/HandlerContext";
 import { MappedParameters } from "../../../src/index";
 import { CommandHandlerMetadata } from "../../../src/metadata/automationMetadata";
+import { GitHubRepoRef } from "../../../src/operations/common/GitHubRepoRef";
 
 describe("class with external parameters metadata reading", () => {
 
@@ -23,7 +24,7 @@ describe("class with external parameters metadata reading", () => {
         assert(md.secrets[0].path === "atomist://some_secret");
     });
 
-    it("should extract metadata from command handler with external parameters", () => {
+    it("should extract metadata from command handler with external nested parameters", () => {
         const h = new AddAtomistSpringAgentWithComposedParameters();
         const md = metadataFromInstance(h) as CommandHandlerMetadata;
         assert(md.parameters.length === 1);
@@ -33,6 +34,74 @@ describe("class with external parameters metadata reading", () => {
         assert(md.mapped_parameters[0].foreign_key === "atomist://github_url");
         assert(md.secrets.length === 1);
         assert(md.secrets[0].name === "secrets.someSecret");
+        assert(md.secrets[0].path === "atomist://some_secret");
+    });
+
+    it("should extract metadata from command handler with direct nested parameters", () => {
+        const h = new AddAtomistSpringAgentWithComposedParametersDirectlyOnHandler();
+        const md = metadataFromInstance(h) as CommandHandlerMetadata;
+        assert(md.parameters.length === 1);
+        assert(md.parameters[0].name === "params.slackTeam");
+        assert(md.mapped_parameters.length === 2);
+        assert(md.mapped_parameters[0].local_key === "params.githubWebUrl");
+        assert(md.mapped_parameters[0].foreign_key === "atomist://github_url");
+        assert(md.secrets.length === 1);
+        assert(md.secrets[0].name === "secrets.someSecret");
+        assert(md.secrets[0].path === "atomist://some_secret");
+    });
+
+    it("should extract metadata from command handler with direct nested parameters and irrelevant fields", () => {
+        const h = new AddAtomistSpringAgentWithComposedParametersDirectlyOnHandlerAndIrrelevantFields();
+        const md = metadataFromInstance(h) as CommandHandlerMetadata;
+        assert(md.parameters.length === 1);
+        assert(md.parameters[0].name === "params.slackTeam");
+        assert(md.mapped_parameters.length === 2);
+        assert(md.mapped_parameters[0].local_key === "params.githubWebUrl");
+        assert(md.mapped_parameters[0].foreign_key === "atomist://github_url");
+        assert(md.secrets.length === 1);
+        assert(md.secrets[0].name === "secrets.someSecret");
+        assert(md.secrets[0].path === "atomist://some_secret");
+    });
+
+    it("should extract metadata from command handler with direct nested parameters and additional simple parameter", () => {
+        const h = new AddAtomistSpringAgentWithComposedParametersDirectlyOnHandlerAndStringParam();
+        const md = metadataFromInstance(h) as CommandHandlerMetadata;
+        assert(md.parameters.length === 2);
+        assert(md.parameters[0].name === "foo");
+        assert(md.parameters[1].name === "params.slackTeam");
+        assert(md.mapped_parameters.length === 2);
+        assert(md.mapped_parameters[0].local_key === "params.githubWebUrl");
+        assert(md.mapped_parameters[0].foreign_key === "atomist://github_url");
+        assert(md.secrets.length === 1);
+        assert(md.secrets[0].name === "secrets.someSecret");
+        assert(md.secrets[0].path === "atomist://some_secret");
+    });
+
+    it("should extract metadata from command handler with direct composed parameters", () => {
+        const h = new AddAtomistSpringAgentWithComposedExternalParameters();
+        const md = metadataFromInstance(h) as CommandHandlerMetadata;
+        assert(md.parameters.length === 2);
+        assert(md.parameters[0].name === "foo");
+        assert(md.parameters[1].name === "args.params.slackTeam");
+        assert(md.mapped_parameters.length === 2);
+        assert(md.mapped_parameters[0].local_key === "args.params.githubWebUrl");
+        assert(md.mapped_parameters[0].foreign_key === "atomist://github_url");
+        assert(md.secrets.length === 1);
+        assert(md.secrets[0].name === "args.secrets.someSecret");
+        assert(md.secrets[0].path === "atomist://some_secret");
+    });
+
+    it("should extract metadata from command handler with inherited and composed parameters", () => {
+        const h = new AddAtomistSpringAgentWithInheritedAndComposedParameters();
+        const md = metadataFromInstance(h) as CommandHandlerMetadata;
+        assert(md.parameters.length === 2);
+        assert(md.parameters[0].name === "foo");
+        assert(md.parameters[1].name === "args.slackTeam");
+        assert(md.mapped_parameters.length === 2);
+        assert(md.mapped_parameters[0].local_key === "args.githubWebUrl");
+        assert(md.mapped_parameters[0].foreign_key === "atomist://github_url");
+        assert(md.secrets.length === 1);
+        assert(md.secrets[0].name === "args.someSecret");
         assert(md.secrets[0].path === "atomist://some_secret");
     });
 
@@ -124,6 +193,117 @@ class AddAtomistSpringAgentWithComposedParameters implements HandleCommand<Compo
         assert(params.params.slackTeam);
         assert(params.params.repo !== undefined,
             `this.repo=[${params.params.repo}],this.slackTeam=[${params.params.slackTeam}],this.githubWebUrl=[${params.params.githubWebUrl}]`);
+        return Promise.resolve({code: 0});
+    }
+}
+
+@CommandHandler("add the Atomist Spring Boot agent to a Spring Boot project")
+@Tags("atomist", "spring")
+class AddAtomistSpringAgentWithComposedParametersDirectlyOnHandler implements HandleCommand {
+
+    public params = new AddAtomistSpringAgentParamsPart();
+
+    public secrets = new AddAtomistSpringAgentSecretsPart();
+
+    public handle(context: HandlerContext, params: this) {
+        console.log(`Invocation with team [${params.params.slackTeam}]`);
+        assert(params.secrets.someSecret);
+        assert(params.params.slackTeam);
+        assert(params.params.repo !== undefined,
+            `this.repo=[${params.params.repo}],this.slackTeam=[${params.params.slackTeam}],this.githubWebUrl=[${params.params.githubWebUrl}]`);
+        return Promise.resolve({code: 0});
+    }
+}
+
+@CommandHandler("add the Atomist Spring Boot agent to a Spring Boot project")
+@Tags("atomist", "spring")
+class AddAtomistSpringAgentWithComposedParametersDirectlyOnHandlerAndIrrelevantFields implements HandleCommand {
+
+    public params = new AddAtomistSpringAgentParamsPart();
+
+    public secrets = new AddAtomistSpringAgentSecretsPart();
+
+    public irrelevantObject = new GitHubRepoRef("a", "b");
+
+    public irrevelantString = "ignore me";
+
+    public handle(context: HandlerContext, params: this) {
+        console.log(`Invocation with team [${params.params.slackTeam}]`);
+        assert(params.secrets.someSecret);
+        assert(params.params.slackTeam);
+        assert(params.params.repo !== undefined,
+            `this.repo=[${params.params.repo}],this.slackTeam=[${params.params.slackTeam}],this.githubWebUrl=[${params.params.githubWebUrl}]`);
+        return Promise.resolve({code: 0});
+    }
+}
+
+@CommandHandler("add the Atomist Spring Boot agent to a Spring Boot project")
+@Tags("atomist", "spring")
+class AddAtomistSpringAgentWithComposedParametersDirectlyOnHandlerAndStringParam implements HandleCommand {
+
+    public params = new AddAtomistSpringAgentParamsPart();
+
+    public secrets = new AddAtomistSpringAgentSecretsPart();
+
+    @Parameter()
+    public foo: string;
+
+    public handle(context: HandlerContext, params: this) {
+        console.log(`Invocation with team [${params.params.slackTeam}]`);
+        assert(params.secrets.someSecret);
+        assert(params.params.slackTeam);
+        assert(params.params.repo !== undefined,
+            `this.repo=[${params.params.repo}],this.slackTeam=[${params.params.slackTeam}],this.githubWebUrl=[${params.params.githubWebUrl}]`);
+        return Promise.resolve({code: 0});
+    }
+}
+
+class ComposeAllAddAtomistSpringAgentParams {
+
+    public params = new AddAtomistSpringAgentParamsPart();
+
+    public secrets = new AddAtomistSpringAgentSecretsPart();
+
+}
+
+@CommandHandler("add the Atomist Spring Boot agent to a Spring Boot project")
+@Tags("atomist", "spring")
+class AddAtomistSpringAgentWithComposedExternalParameters implements HandleCommand {
+
+    public args = new ComposeAllAddAtomistSpringAgentParams();
+
+    @Parameter()
+    public foo: string;
+
+    public handle(context: HandlerContext, params: this) {
+        console.log(`Invocation with team [${params.args.params.slackTeam}]`);
+        assert(params.args.secrets.someSecret);
+        assert(params.args.params.slackTeam);
+        assert(params.args.params.repo !== undefined);
+        return Promise.resolve({code: 0});
+    }
+}
+
+class InheritedAndComposeParams extends AddAtomistSpringAgentParamsPart {
+
+    @Secret("atomist://some_secret")
+    public someSecret: string;
+}
+
+@CommandHandler("add the Atomist Spring Boot agent to a Spring Boot project")
+@Tags("atomist", "spring")
+class AddAtomistSpringAgentWithInheritedAndComposedParameters implements HandleCommand {
+
+    public args = new InheritedAndComposeParams();
+
+    @Parameter()
+    public foo: string;
+
+    public handle(context: HandlerContext, params: this) {
+        console.log(`Invocation with team [${params.args.slackTeam}]`);
+        assert(params.args.someSecret);
+        assert(params.args.slackTeam);
+        assert(params.args.repo !== undefined);
         return Promise.resolve({code: 0});
     }
 }
