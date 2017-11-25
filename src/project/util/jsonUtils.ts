caf9f9b526427f13904de44c1b7228c3fc8b8176
Rod Johnson <rod@atomist.com>
2017-11-26 09:22:24 +1100

    JSON editing using parsing and stringification

---
+++ src/project/util/jsonUtils.ts
@@ -0,0 +1,65 @@
+import { ProjectAsync } from "../Project";
+import { doWithFiles } from "./projectUtils";
+import { logger } from "../../internal/util/logger";
+
+export type JsonManipulation = (jsonObj: any) => void;
+
+/**
+ * Manipulate the contents of the given JSON file within the project,
+ * using its object form and writing back using the same formatting.
+ * See the manipulate function.
+ * @param {P} p
+ * @param {string} jsonPath JSON file path. This function will do nothing
+ * without error if the file is ill-formed or not found.
+ * @param {JsonManipulation} manipulation
+ * @return {Promise<P extends ProjectAsync>}
+ */
+export function doWithJson<M, P extends ProjectAsync = ProjectAsync>(p: P,
+                                                                     jsonPath: string,
+                                                                     manipulation: JsonManipulation): Promise<P> {
+    return doWithFiles(p, jsonPath, file => {
+        return file.getContent()
+            .then(content => {
+                const newContent = manipulate(content, manipulation, jsonPath);
+                return file.setContent(newContent)
+                    .then(() => p);
+            });
+    });
+}
+
+const spacePossibilities = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, " ", "  ", "\t"];
+
+/**
+ * Update the object form of the given JSON content and write
+ * it back with minimal changes
+ * @param {string} jsonIn
+ * @param {(jsonObj: any) => Object} manipulation
+ * @return {string}
+ */
+export function manipulate(jsonIn: string, manipulation: JsonManipulation, context: string = ""): string {
+    if (!jsonIn) {
+        return jsonIn;
+    }
+
+    try {
+        const obj = JSON.parse(jsonIn);
+
+        let space: number | string = 0;
+        for (const sp of spacePossibilities) {
+            const maybe = JSON.stringify(obj, null, sp);
+            if (jsonIn === maybe) {
+                console.log(`Definitely inferred space as [${sp}]`);
+                space = sp;
+                break;
+            }
+        }
+
+        console.log(`Inferred space is [${space}]`);
+
+        manipulation(obj);
+        return JSON.stringify(obj, null, space);
+    } catch (e) {
+        logger.warn("Syntax error parsing supposed JSON (%s). Context:[%s]. Alleged JSON:\n%s", e, context, jsonIn);
+        return jsonIn;
+    }
+}
---
+++ test/project/util/jsonUtilsTest.ts
@@ -0,0 +1,121 @@
+import "mocha";
+
+import * as assert from "power-assert";
+import { InMemoryProject } from "../../../src/project/mem/InMemoryProject";
+import { doWithJson, manipulate } from "../../../src/project/util/jsonUtils";
+
+describe("jsonUtils", () => {
+
+    describe("manipulate", () => {
+
+        it("should handle undefined", () => {
+            assert(manipulate(undefined, o => null) === undefined);
+        });
+
+        it("should handle null", () => {
+            assert(manipulate(null, o => null) === null);
+        });
+
+        it("should handle ill formed string", () => {
+            const s = "not json at all";
+            assert(manipulate(s, o => null) === s);
+        });
+
+        it("should handle simple number", () => {
+            const json = simpleJson(25);
+            const transformed = manipulate(json, o => o.y = 31);
+            assert(transformed === simpleJson(31),
+                "\n" + json + "\n" + transformed);
+        });
+
+        it("should handle real content", () => {
+            const json = fromPackageJson("@atomist/test");
+            const desired = fromPackageJson("newer");
+            const transformed = manipulate(json, o => o.name = "newer");
+            assert(transformed === desired,
+                "\n" + json + "\n" + transformed);
+        });
+
+        it("should add structured elements to real content", () => {
+            const json = fromPackageJson("@atomist/test");
+            const transformed = manipulate(json,
+                    o => o.extras = [
+                        { greg: "CEO" },
+                        { nadine: "GC"},
+                    ]);
+            assert(transformed.includes('"greg": "CEO"'));
+            // Should succeed
+            JSON.parse(transformed);
+        });
+
+    });
+
+    describe("doWithJson", () => {
+
+        it("should handle no such file", done => {
+            const notJson = "bar";
+
+            const p = InMemoryProject.of({path: "thing.json", content: notJson});
+            doWithJson(p, "not_there.json", o => null)
+                .then(() => {
+                    done();
+                }).catch(done);
+        });
+
+        it("should handle ill formed file", done => {
+            const notJson = "bar";
+
+            const p = InMemoryProject.of({path: "thing.json", content: notJson});
+
+            doWithJson(p, "thing.json", o => null)
+                .then(() => {
+                    const transformed = p.findFileSync("thing.json").getContentSync();
+                    assert(transformed === notJson);
+                    done();
+                }).catch(done);
+        });
+
+        it("should handle simple number", done => {
+            const simple = simpleJson(25);
+            const desired = simpleJson(31);
+            const manipulation = o => o.y = 31;
+
+            const p = InMemoryProject.of({path: "thing.json", content: simple});
+
+            doWithJson(p, "thing.json", manipulation)
+                .then(() => {
+                    const transformed = p.findFileSync("thing.json").getContentSync();
+                    assert(transformed === desired,
+                        "\n" + simple + "\n" + transformed);
+                    done();
+                }).catch(done);
+        });
+    });
+
+});
+
+function simpleJson(b: number) {
+    return `{"x":5,"y":${b}}`;
+}
+
+function fromPackageJson(name: string) {
+    return `{
+  "name": "${name}",
+  "version": "0.3.5",
+  "description": "Atomist automation client for running command and event handlers",
+  "author": "Atomist, Inc.",
+  "license": "GPL-3.0",
+  "homepage": "https://github.com/atomist/automation-client-ts#readme",
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/atomist/automation-client-ts.git"
+  },
+  "keywords": [
+    "atomist",
+    "automation"
+  ],
+  "bugs": {
+    "url": "https://github.com/atomist/automation-client-ts/issues"
+  }
+}`;
+}
