fc6ca7ea1aec0c3a825dd9095688e7e41faf5b56
Rod Johnson <rod@atomist.com>
2017-11-27 16:24:09 -0800

    Additional tests for RepoLoader usage in editor handlers

---
+++ test/operations/edit/VerifyEditMode.ts
@@ -0,0 +1,24 @@
+import { HandlerContext } from "../../../src/HandlerContext";
+import { CustomExecutionEditMode } from "../../../src/operations/edit/editModes";
+import { EditResult, ProjectEditor } from "../../../src/operations/edit/projectEditor";
+import { Project } from "../../../src/project/Project";
+
+/**
+ * EditMode implementation that allows verification of the
+ * resulting state of the project
+ */
+export class VerifyEditMode implements CustomExecutionEditMode {
+
+    public message = "foo";
+
+    constructor(private assertions: (p: Project) => void) {
+    }
+
+    public edit<P>(p: Project, action: ProjectEditor<P>, context: HandlerContext, parameters: P): Promise<EditResult> {
+        return action(p, context, parameters)
+            .then(er => {
+                this.assertions(p);
+                return er;
+            });
+    }
+}
--- test/operations/edit/editorHandlerTest.ts
+++ test/operations/edit/editorHandlerTest.ts
@@ -3,10 +3,13 @@ import "mocha";
 import * as assert from "power-assert";
 import { metadataFromInstance } from "../../../src/internal/metadata/metadataReading";
 import { CommandHandlerMetadata } from "../../../src/metadata/automationMetadata";
-import { fromListRepoFinder } from "../../../src/operations/common/fromProjectList";
+import { fromListRepoFinder, fromListRepoLoader } from "../../../src/operations/common/fromProjectList";
+import { SimpleRepoId } from "../../../src/operations/common/RepoId";
 import { BaseEditorParameters } from "../../../src/operations/edit/BaseEditorParameters";
 import { editorHandler } from "../../../src/operations/edit/editorToCommand";
+import { InMemoryProject } from "../../../src/project/mem/InMemoryProject";
 import { BuildableAutomationServer } from "../../../src/server/BuildableAutomationServer";
+import { VerifyEditMode } from "./VerifyEditMode";
 
 describe("editorHandler", () => {
 
@@ -56,7 +59,7 @@ describe("editorHandler", () => {
         s.registerCommandHandler(() => h);
         s.invokeCommand({
             name: "editor",
-            args: [{name: "slackTeam", value: "T1691" }, {name: "owner", value: "foo"}],
+            args: [{name: "slackTeam", value: "T1691"}, {name: "owner", value: "foo"}],
             secrets: [{name: "github://user_token?scopes=repo,user", value: "antechinus"}],
         }, {
             teamId: "T666",
@@ -64,6 +67,38 @@ describe("editorHandler", () => {
             messageClient: null,
         })
             .then(_ => done(), done);
+    });
+
+    it("should use custom repo loader and verify result", done => {
+        const proj = InMemoryProject.from(new SimpleRepoId("a", "b"));
+        const h = editorHandler(params => {
+                assert(!!params);
+                assert(params.owner === "foo");
+                return p => p.addFile("Thing", "1");
+            },
+            BaseEditorParameters,
+            "editor", {
+                repoFinder: fromListRepoFinder([proj]),
+                repoLoader: () => fromListRepoLoader([proj]),
+                editMode: new VerifyEditMode(p => {
+                    assert(p.findFileSync("Thing").getContentSync() === "1");
+                }),
+            });
+        const s = new BuildableAutomationServer({name: "foobar", version: "1.0.0", teamIds: ["bar"], keywords: []});
+        s.registerCommandHandler(() => h);
+        s.invokeCommand({
+            name: "editor",
+            args: [{name: "slackTeam", value: "T1691"}, {name: "owner", value: "foo"}],
+            secrets: [{name: "github://user_token?scopes=repo,user", value: "antechinus"}],
+        }, {
+            teamId: "T666",
+            correlationId: "555",
+            messageClient: null,
+        })
+            .then(_ => {
+                assert(proj.findFileSync("Thing").getContentSync() === "1");
+                done();
+            }, done);
     });
 
 });
