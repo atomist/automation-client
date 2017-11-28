f941f5b97cdb63845f301a970ab5bdc5517f5fe0
Rod Johnson <rod@atomist.com>
2017-11-28 12:25:04 -0800

    SourceLocation inference

---
+++ src/project/util/sourceLocationUtils.ts
@@ -0,0 +1,25 @@
+
+import { SourceLocation } from "../../operations/common/SourceLocation";
+
+import { File, isFile } from "../../project/File";
+
+/**
+ * Find the given source location within this project
+ * @param {string} f file info: Path or File
+ * @param {string} content
+ * @param {number} offset
+ * @return {SourceLocation}
+ */
+export function toSourceLocation(f: string | File, content: string, offset: number): SourceLocation {
+    if (!content || offset < 0 || offset > content.length - 1) {
+        return undefined;
+    }
+
+    const lines = content.substr(0, offset).split("\n");
+    return {
+        path: isFile(f) ? f.path : f,
+        lineFrom1: lines.length,
+        columnFrom1: lines[lines.length - 1].length + 1,
+        offset,
+    };
+}
