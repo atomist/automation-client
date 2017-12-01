83b5708abdcbfb4017bcf5810383420989cd45e0
David Dooling <david@atomist.com>
2017-12-01 08:44:03 -0600

    Optionally add Atomist webhook to create repo
    When generating a project, optionally create a webhook on the repo
    with the Atomist webhook URL for that team.  Will add tests for this
    functionality when #137 is complete.

--- src/operations/generate/support/addAtomistWebhook.ts
+++ src/operations/generate/support/addAtomistWebhook.ts
@@ -1,6 +1,9 @@
-import { ActionResult, successOn } from "../../../action/ActionResult";
+import * as util from "util";
+
+import { ActionResult, failureOn, successOn } from "../../../action/ActionResult";
 import { logger } from "../../../internal/util/logger";
 import { GitProject } from "../../../project/git/GitProject";
+import { addRepoWebhook, GitHubRepoWebhookPayload } from "../../../util/gitHub";
 import { isGitHubRepoRef } from "../../common/GitHubRepoRef";
 import { ProjectAction } from "../../common/projectAction";
 import { BaseSeedDrivenGeneratorParameters } from "../BaseSeedDrivenGeneratorParameters";
@@ -13,24 +16,44 @@ import { BaseSeedDrivenGeneratorParameters } from "../BaseSeedDrivenGeneratorPar
  * @return {any}
  */
 export const addAtomistWebhook: ProjectAction<BaseSeedDrivenGeneratorParameters, GitProject> =
-    (p, params) => {
-        if (!params.addAtomistWebhook) {
-            return Promise.resolve(successOn(p));
-        } else {
-            return addWebhook(p)
-                .then(r => ({
-                    ...r,
-                    target: p,
-                }));
-        }
-    };
+    (p, params) => addWebhook(p, params)
+        .then(r => ({
+            ...r,
+            target: p,
+        }));
 
-function addWebhook(p: GitProject): Promise<ActionResult<any>> {
-    if (isGitHubRepoRef(p.id)) {
-        // TODO GitHub info is here
-        return Promise.resolve(successOn(p));
-    } else {
-        logger.warn("Unable to add Atomist web hook: Not a GitHub repo [%j]", p.id);
+function addWebhook(p: GitProject, params: BaseSeedDrivenGeneratorParameters): Promise<ActionResult<any>> {
+    if (!params.addAtomistWebhook) {
         return Promise.resolve(successOn(p));
     }
+
+    function logAndFail(fmt: string, ...args: any[]): Promise<ActionResult<GitProject>> {
+        const msg = util.format(fmt, ...args);
+        logger.error(msg);
+        return Promise.resolve(failureOn(p, new Error(msg), { name: "addWebhook" }));
+    }
+
+    if (!isGitHubRepoRef(p.id)) {
+        return logAndFail("Unable to add Atomist web hook: Not a GitHub repo [%j]", p.id);
+    }
+    if (!params.webhookUrl) {
+        return logAndFail("Requested to add webhook but no URL provided");
+    }
+    if (!params.target.githubToken) {
+        return logAndFail("Requested to add webhook but no GitHub token provided");
+    }
+
+    const payload: GitHubRepoWebhookPayload = {
+        name: "web",
+        events: ["*"],
+        active: true,
+        config: {
+            url: params.webhookUrl,
+            content_type: "json",
+        },
+    };
+    return addRepoWebhook(params.target.githubToken, p.id, payload)
+        .then(() => Promise.resolve(successOn(p)), err => {
+            return logAndFail("Failed to install Atomist webhook on %s/%s: %j", p.id.owner, p.id.repo, err);
+        });
 }
