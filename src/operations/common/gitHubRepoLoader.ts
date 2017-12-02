bb5aa39932645162b7d05c29a3ade250e5fbb677
Rod Johnson <rod@atomist.com>
2017-12-01 18:32:44 -0800

    Fixed compile error

--- src/operations/common/gitHubRepoLoader.ts
+++ src/operations/common/gitHubRepoLoader.ts
@@ -1,4 +1,5 @@
 import { GitCommandGitProject } from "../../project/git/GitCommandGitProject";
+import { GitProject } from "../../project/git/GitProject";
 import { GitHubRepoRef, isGitHubRepoRef } from "./GitHubRepoRef";
 import { ProjectOperationCredentials } from "./ProjectOperationCredentials";
 import { RepoLoader } from "./repoLoader";
@@ -9,7 +10,7 @@ import { RepoLoader } from "./repoLoader";
  * @return function to materialize repos
  * @constructor
  */
-export function gitHubRepoLoader(credentials: ProjectOperationCredentials): RepoLoader {
+export function gitHubRepoLoader(credentials: ProjectOperationCredentials): RepoLoader<GitProject> {
     return repoId => {
         // Default it if it isn't already a GitHub repo ref
         const gid = isGitHubRepoRef(repoId) ? repoId : new GitHubRepoRef(repoId.owner, repoId.repo, repoId.sha);
