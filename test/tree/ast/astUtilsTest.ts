cf735f6e3ae1393fc0388b47f2fff1e4e64ce8dd
Jessica Kerr <jessitron@gmail.com>
2017-12-02 09:25:50 -0800

    Flush doubles the file content
    This test illustrates a failure, in updating a tree node and then flushing.

--- test/tree/ast/astUtilsTest.ts
+++ test/tree/ast/astUtilsTest.ts
@@ -81,3 +81,67 @@ describe("astUtils", () => {
     });
 
 });
+
+describe("findMatches in action", () => {
+    it("can flush this and it works", done => {
+        const Before = `import { GitCommandGitProject } from "../../project/git/GitCommandGitProject";
+import { GitProject } from "../../project/git/GitProject";
+import { GitHubRepoRef, isGitHubRepoRef } from "./GitHubRepoRef";
+import { ProjectOperationCredentials } from "./ProjectOperationCredentials";
+import { RepoLoader } from "./repoLoader";
+
+/**
+ * Materialize from github
+ * @param credentials provider token
+ * @return function to materialize repos
+ * @constructor
+ */
+export function gitHubRepoLoader(credentials: ProjectOperationCredentials): RepoLoader<GitProject> {
+    return repoId => {
+        // Default it if it isn't already a GitHub repo ref
+        const gid = isGitHubRepoRef(repoId) ? repoId : new GitHubRepoRef(repoId.owner, repoId.repo, repoId.sha);
+        return GitCommandGitProject.cloned(credentials, gid);
+    };
+}`
+
+        const mutableProject = InMemoryProject.of({path: "src/gitHubRepoLoader.ts", content: Before});
+
+        const After = `import { GitCommandGitProject } from "../../project/git/GitCommandGitProject";
+import { GitProject } from "../../project/git/GitProject";
+import { GitHubRepoRef, isGitHubRepoRef } from "./GitHubRepoRef";
+import { ProjectOperationCredentials } from "./ProjectOperationCredentials";
+import { RepoLoader } from "./repoLoader";
+
+/**
+ * Materialize from github
+ * @param credentials provider token
+ * @return function to materialize repos
+ * @constructor
+ */
+export function gitHubRepoLoader(context: HandlerContext, credentials: ProjectOperationCredentials): RepoLoader<GitProject> {
+    return repoId => {
+        // Default it if it isn't already a GitHub repo ref
+        const gid = isGitHubRepoRef(repoId) ? repoId : new GitHubRepoRef(repoId.owner, repoId.repo, repoId.sha);
+        return GitCommandGitProject.cloned(credentials, gid);
+    };
+}`;
+
+        return findMatches(mutableProject, TypeScriptES6FileParser, "**/" + "src/gitHubRepoLoader.ts",
+            `//FunctionDeclaration[/Identifier[@value='gitHubRepoLoader']]`)
+            .then(matches => {
+                const enclosingFunction = matches[0];
+                const newValue = enclosingFunction.$value.replace(
+                    /gitHubRepoLoader\s*\(/g,
+                    `gitHubRepoLoader(context: HandlerContext, `);
+                enclosingFunction.$value = newValue;
+            })
+            .then(() => mutableProject.flush())
+            .then(() => {
+                const modified = mutableProject.findFileSync("src/gitHubRepoLoader.ts").getContentSync();
+
+                assert.equal(modified, After, modified);
+
+            }).then(() => done(), done)
+
+    }).timeout(20000)
+});
