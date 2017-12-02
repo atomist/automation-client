6a29f6609547d56122cc2c3a62dd38d8275efa2c
Rod Johnson <rod@atomist.com>
2017-12-01 18:10:34 -0800

    Avoid error on passing in non-GitHub RepoRef to githubRepoLoader

--- src/operations/common/gitHubRepoLoader.ts
+++ src/operations/common/gitHubRepoLoader.ts
@@ -1,6 +1,5 @@
 import { GitCommandGitProject } from "../../project/git/GitCommandGitProject";
-import { GitProject } from "../../project/git/GitProject";
-import { GitHubRepoRef } from "./GitHubRepoRef";
+import { GitHubRepoRef, isGitHubRepoRef } from "./GitHubRepoRef";
 import { ProjectOperationCredentials } from "./ProjectOperationCredentials";
 import { RepoLoader } from "./repoLoader";
 
@@ -10,8 +9,10 @@ import { RepoLoader } from "./repoLoader";
  * @return function to materialize repos
  * @constructor
  */
-export function gitHubRepoLoader(credentials: ProjectOperationCredentials): RepoLoader<GitProject> {
-    return (repoId: GitHubRepoRef) => {
-        return GitCommandGitProject.cloned(credentials, repoId);
+export function gitHubRepoLoader(credentials: ProjectOperationCredentials): RepoLoader {
+    return repoId => {
+        // Default it if it isn't already a GitHub repo ref
+        const gid = isGitHubRepoRef(repoId) ? repoId : new GitHubRepoRef(repoId.owner, repoId.repo, repoId.sha);
+        return GitCommandGitProject.cloned(credentials, gid);
     };
 }
