6ef2ab322934d5c11b81a1f745981519c07ce0aa
Rod Johnson <rod@atomist.com>
2017-12-04 21:11:37 -0800

    Added tagger first class concept

---
+++ src/operations/tagger/taggerHandler.ts
@@ -0,0 +1,103 @@
+import { Project } from "../../project/Project";
+
+import { ActionResult } from "../../action/ActionResult";
+import { HandleCommand } from "../../HandleCommand";
+import { HandlerContext } from "../../HandlerContext";
+import { commandHandlerFrom, OnCommand, ParametersConstructor } from "../../onCommand";
+import { CommandDetails } from "../CommandDetails";
+import { allReposInTeam } from "../common/allReposInTeamRepoFinder";
+import { defaultRepoLoader } from "../common/defaultRepoLoader";
+import { EditorOrReviewerParameters } from "../common/params/BaseEditorOrReviewerParameters";
+import { ProjectOperationCredentials } from "../common/ProjectOperationCredentials";
+import { AllRepos, andFilter, RepoFilter } from "../common/RepoFilter";
+import { RepoFinder } from "../common/repoFinder";
+import { RepoLoader } from "../common/repoLoader";
+import { doWithAllRepos } from "../common/repoUtils";
+
+export interface Tags {
+
+    tags: string[];
+}
+
+export type Tagger<P extends EditorOrReviewerParameters = EditorOrReviewerParameters> =
+    (p: Project, context: HandlerContext, params?: P) => Promise<Tags>;
+
+export type TagRouter = (tags: Tags, ctx: HandlerContext) => Promise<ActionResult<Tags>>;
+
+export interface TaggerCommandDetails<PARAMS extends EditorOrReviewerParameters> extends CommandDetails<PARAMS> {
+
+    tagRouter: TagRouter;
+
+    repoFilter?: RepoFilter;
+
+}
+
+function defaultDetails(name: string): TaggerCommandDetails<EditorOrReviewerParameters> {
+    return {
+        tagRouter: MessageClientTagRouter,
+        description: name,
+    };
+}
+
+export const MessageClientTagRouter: TagRouter = (tags, ctx) =>
+    ctx.messageClient.respond("Tags: " + tags.tags.join());
+
+/**
+ * Create a handle function that tags one or many repos, following AllReposByDefaultParameters
+ * @param tagger tagger function
+ * @param {ParametersConstructor<PARAMS>} factory
+ * @param {string} name
+ * @param {string} details object allowing customization beyond reasonable defaults
+ * @return {HandleCommand}
+ */
+export function taggerHandler<PARAMS extends EditorOrReviewerParameters>(tagger: Tagger<PARAMS>,
+                                                                         factory: ParametersConstructor<PARAMS>,
+                                                                         name: string,
+                                                                         details: Partial<TaggerCommandDetails<PARAMS>> = {}): HandleCommand {
+    const detailsToUse: TaggerCommandDetails<PARAMS> = {
+        ...defaultDetails(name),
+        ...details,
+    };
+    return commandHandlerFrom(tagOneOrMany(tagger, name, detailsToUse),
+        factory,
+        name,
+        detailsToUse.description, detailsToUse.intent, detailsToUse.tags);
+}
+
+/**
+ * If owner and repo are required, tag just one repo. Otherwise tag all repos
+ * in the present team
+ */
+function tagOneOrMany<PARAMS extends EditorOrReviewerParameters>(tagger: Tagger<PARAMS>,
+                                                                 name: string,
+                                                                 details: TaggerCommandDetails<PARAMS>): OnCommand<PARAMS> {
+    return (ctx: HandlerContext, parameters: PARAMS) => {
+        const credentials = {token: parameters.targets.githubToken};
+        const repoFinder: RepoFinder = parameters.targets.repoRef ?
+            () => Promise.resolve([parameters.targets.repoRef]) :
+            details.repoFinder;
+        return tagAll(ctx, credentials, tagger, parameters,
+            repoFinder,
+            andFilter(parameters.targets.test, details.repoFilter),
+            !!details.repoLoader ? details.repoLoader(parameters) : undefined)
+            .then((tags: Tags[]) => {
+                return Promise.all(tags
+                    .filter(pr => tags.length > 0)
+                    .map(t => details.tagRouter(t, ctx)));
+            });
+    };
+}
+
+function tagAll<P extends EditorOrReviewerParameters>(ctx: HandlerContext,
+                                                      credentials: ProjectOperationCredentials,
+                                                      tagger: Tagger<P>,
+                                                      parameters?: P,
+                                                      repoFinder: RepoFinder = allReposInTeam(),
+                                                      repoFilter: RepoFilter = AllRepos,
+                                                      repoLoader: RepoLoader =
+                                                          defaultRepoLoader(
+                                                              credentials)): Promise<Tags[]> {
+    return doWithAllRepos(ctx, credentials,
+        p => tagger(p, ctx, parameters), parameters,
+        repoFinder, repoFilter, repoLoader);
+}
