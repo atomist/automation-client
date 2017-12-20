ecbe3883fbfee9e6533190e89b957cb35d883553
Christian Dupuis <cd@atomist.com>
2017-12-20 14:03:54 +0100

    add github org security check

--- src/secured.ts
+++ src/secured.ts
@@ -54,6 +54,67 @@ export function slackUser(maker: Maker<HandleCommand>, ...users: string[]): () =
 }
 
 /**
+ * Protect the given HandleCommand by only allowing members of a certain GitHub Organization
+ * @param {Maker<HandleCommand>} maker
+ * @param {string} team
+ * @returns {() => HandleCommand}
+ */
+export function githubOrg(maker: Maker<HandleCommand>, org: string): () => HandleCommand {
+    return () => {
+        const command = toFactory(maker)();
+        declareMappedParameter(command, "__atomist_slack_user", MappedParameters.SlackUser, true);
+        declareSecret(command, "__atomist_user_token", Secrets.userToken("read:org"));
+        const handleMethod = command.handle;
+        command.handle = (ctx: HandlerContext) => {
+
+            const user = (command as any).__atomist_slack_user;
+            const token = (command as any).__atomist_user_token;
+
+            return ctx.graphClient.executeQuery(GitHubIdQuery,
+                { teamId: ctx.teamId, chatId: user })
+                .then(result => {
+                    const login = _.get(result, "ChatTeam[0].members[0].person.gitHubId.login");
+
+                    return isGitHubOrgMember(org, login, token)
+                        .then(isOrgMember => {
+                            if (isOrgMember === true) {
+                                return handleMethod.bind(command)(ctx);
+                            } else {
+                                return sendUnauthorized(ctx);
+                            }
+                        })
+                        .catch(err => {
+                            return sendUnauthorized(ctx);
+                        });
+                });
+        };
+        return command;
+    };
+}
+
+function isGitHubOrgMember(org: string, login: string, token: string): Promise<boolean> {
+    if (login) {
+
+        const config = {
+            headers: {
+                Authorization: `token ${token}`,
+                Accept: "application/vnd.github.hellcat-preview+json",
+            },
+        };
+
+        return axios.get(`https://api.github.com/orgs/${org}/members/${login}`, config)
+            .then(result => {
+                return result.status === 204;
+            })
+            .catch(() => {
+                return false;
+            });
+    } else {
+        return Promise.resolve(false);
+    }
+}
+
+/**
  * Protect the given HandleCommand by only allowing members of a certain GitHub team
  * @param {Maker<HandleCommand>} maker
  * @param {string} team
