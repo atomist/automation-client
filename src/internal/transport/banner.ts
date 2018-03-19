07e69b1e5991a3097507bb07cdf83a27171af9ed
Christian Dupuis <cd@atomist.com>
2018-03-19 15:49:29 +0100

    fix spacing

--- src/internal/transport/banner.ts
+++ src/internal/transport/banner.ts
@@ -29,8 +29,8 @@ export function banner(registration: RegistrationConfirmation,
 
         const urls = automations.team_ids.map(t => {
             return `
-  ${chalk.grey("Url")}  ${chalk.underline(`https://app.atomist.com/workspace/${t}/automations`)}
-  ${chalk.grey("GraphiQL")}  ${chalk.underline(`https://app.atomist.com/workspace/${t}/graphql`)}`;
+  ${chalk.grey("Url")} ${chalk.underline(`https://app.atomist.com/workspace/${t}/automations`)}
+  ${chalk.grey("GraphiQL")} ${chalk.underline(`https://app.atomist.com/workspace/${t}/graphql`)}`;
         });
 
         /* tslint:disable */
@@ -39,7 +39,7 @@ ${res}
   ${chalk.grey("Version")} ${automations.version}${gitInfo.git ? `  ${chalk.grey("Sha")} ${gitInfo.git.sha.slice(0, 7)}  ${chalk.grey("Repository")} ${gitInfo.git.repository}` : ""}
   ${automations.groups && automations.groups.length > 0 ? `${chalk.grey("Groups")} all` : `${chalk.grey(`${automations.team_ids.length > 1 ? "Teams" : "Team"}`)} ${automations.team_ids.join(", ")}`}  ${chalk.grey("Policy")} ${automations.policy ? automations.policy : "ephemeral"}  ${chalk.grey("Cluster")} ${runningAutomationClient.configuration.cluster.enabled ? "enabled" : "disabled"}
   ${chalk.grey(automations.commands.length === 1 ? "Command" : "Commands")} ${automations.commands.length}  ${chalk.grey(automations.events.length === 1 ? "Event" : "Events")} ${automations.events.length}  ${chalk.grey(automations.ingesters.length === 1 ? "Ingester" : "Ingesters")} ${automations.ingesters.length}
-  ${chalk.grey("JWT")}  ${registration.jwt}
+  ${chalk.grey("JWT")} ${registration.jwt}
 ${urls}
 `;      /* tslint:enable */
         logger.info(msg);
