6d3706b8b0dda70dde22d385ae8762d42a2c2c17
David Dooling <david@atomist.com>
2018-04-03 10:06:39 -0500

    Add reduceResults
    Add reduceResults, useful in event handlers.  Add TypeDoc and all
    handler result exports to index.ts.

--- src/HandlerResult.ts
+++ src/HandlerResult.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright © 2018 Atomist, Inc.
+ *
+ * See LICENSE file.
+ */
+
 /**
  * Result of a handler. Can be extended to include further,
  * command-specific, data
@@ -27,6 +33,9 @@ export interface RedirectResult extends HandlerResult {
     redirect: string;
 }
 
+/**
+ * Handler result for failing handler.
+ */
 export interface HandlerError extends HandlerResult {
 
     /**
@@ -35,22 +44,59 @@ export interface HandlerError extends HandlerResult {
     stack?: any;
 }
 
+/**
+ * Default success result.
+ */
 export const Success: HandlerResult = {
     code: 0,
 };
 
+/**
+ * Default success result wrapped in a promise.
+ */
 export const SuccessPromise = Promise.resolve(Success);
 
+/**
+ * Default failure result.
+ */
 export const Failure: HandlerResult = {
     code: 1,
 };
 
+/**
+ * Default failure result wrapped in a promise.
+ */
 export const FailurePromise = Promise.resolve(Failure);
 
+/**
+ * Function that returns a handler failure result.
+ */
 export function failure(err: Error): HandlerError {
     return { code: 1, message: err.message, stack: err.stack };
 }
 
+/**
+ * Function that returns a handler success result.
+ */
 export function success(): HandlerResult {
     return Success;
+}
+
+/**
+ * Combine an array of HandlerResults into a single HandlerResult.
+ * Each HandlerResult.code is summed into the final, single value and
+ * messages are concatenated, separated by a semicolon (;).  Useful to
+ * combine the return value from calling Promise.all on the array of
+ * events sent to an event handler.
+ *
+ * @param results array of HandlerResults
+ * @return single, combined result
+ */
+export function reduceResults(results: HandlerResult[]): HandlerResult {
+    return results.reduce((acc, cur) => {
+        return {
+            code: acc.code + cur.code,
+            message: (cur.message) ? ((acc.message) ? `${acc.message}; ${cur.message}` : cur.message) : acc.message,
+        };
+    }, Success);
 }
