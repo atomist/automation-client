eb26ddbf2bad7dee0af3fd1ba89f6391e5249162
David Dooling <david@atomist.com>
2018-04-03 08:52:19 -0500

    Remove previously deprecate CLIs
    Remove the deprecated command-line utilities superseded by the
    combined `atomist` CLI.

--- src/atomist-cli.ts
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-import * as child_process from "child_process";
-import * as process from "process";
-console.warn("atomist-cli is deprecated, use 'atomist' instead");
-child_process.execFileSync("atomist", process.argv, { env: process.env, stdio: "inherit" });
--- src/atomist-config.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as child_process from "child_process";
-import * as process from "process";
-console.warn("atomist-config is deprecated, use 'atomist config' instead");
-process.argv.unshift("config");
-child_process.execFileSync("atomist", process.argv, { env: process.env, stdio: "inherit" });
