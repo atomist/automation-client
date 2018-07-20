2fea00820fd1c453120b78635173c29482520c60
Rod Johnson <rod@atomist.com>
2018-07-20 12:04:09 +1000

    Copy messageClientFactory

--- src/automationClient.ts
+++ src/automationClient.ts
@@ -202,6 +202,7 @@ export class AutomationClient {
             endpoint: {
                 graphql: this.configuration.endpoints.graphql,
             },
+            messageClientFactory: this.configuration.http.messageClientFactory,
         };
         this.httpServer = new ExpressServer(
             this.automations,
