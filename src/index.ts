33d37ce20b57c73e29ff54259bb565f7c3bb33e0
David Dooling <david@atomist.com>
2018-07-31 14:33:09 -0500

    Add exports used by seed and cli
    Add exports used by automation-seed-ts and the cli to index.ts.
    
    Put linting at end of build package script.  Add TypeDoc generation to
    build package script.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,27 @@
+/*
+ * Copyright © 2018 Atomist, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 export {
     Configuration,
     configurationValue,
+    getUserConfig,
+    resolveWorkspaceIds,
+    userConfigPath,
+    writeUserConfig,
 } from "./configuration";
 
 export {
@@ -48,7 +69,19 @@ import * as GraphQL from "./graph/graphQL";
 
 export { GraphQL };
 
-export { logger } from "./internal/util/logger";
+export {
+    obtainGitInfo,
+} from "./internal/env/gitInfo";
+
+export {
+    Arg,
+    CommandInvocation,
+} from "./internal/invoker/Payload";
+
+export {
+    logger,
+    LoggingConfig,
+} from "./internal/util/logger";
 
 export {
     buildEnum,
@@ -64,6 +97,7 @@ export {
     AtomistLinkImage,
     AtomistWebhookType,
     postAtomistWebhook,
+    webhookBaseUrl,
 } from "./atomistWebhook";
 
 export { automationClientInstance } from "./globals";
