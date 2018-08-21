83aa7f46fe20b817da9cbeb269ad6e1b276bc5bf
Christian Dupuis <cd@atomist.com>
2018-08-21 19:28:09 +0200

    Print nicer output for startup errors

--- src/bin/start.ts
+++ src/bin/start.ts
@@ -13,7 +13,18 @@ try {
         })
         .then(configuration => automationClient(configuration).run())
         .catch(e => {
-            console.error(e);
+            if (e instanceof Error) {
+                if (e.stack && e.stack.includes(e.message)) {
+                    console.error(e.stack);
+                } else if (e.stack) {
+                    console.error(e.message);
+                    console.error(e.stack);
+                } else {
+                    console.error(e.message);
+                }
+            } else {
+                console.error(e);
+            }
             process.exit(1);
         });
 } catch (e) {
