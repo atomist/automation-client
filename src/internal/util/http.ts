15113d962ac25c6521728c5b1c8b4c8f8afcdf0f
Christian Dupuis <cd@atomist.com>
2018-08-21 18:19:28 +0200

    Fix proxy auth configuration

--- src/internal/util/http.ts
+++ src/internal/util/http.ts
@@ -1,18 +1,31 @@
 import { AxiosRequestConfig } from "axios";
+import { UrlWithStringQuery } from "url";
 import * as url from "url";
 
 export function configureProxy(config: AxiosRequestConfig): AxiosRequestConfig {
     if (process.env.HTTPS_PROXY || process.env.https_proxy) {
         const proxy = process.env.HTTPS_PROXY || process.env.https_proxy;
         const proxyOpts = url.parse(proxy);
+
         config.proxy = {
             host: proxyOpts.hostname,
             port: +proxyOpts.port,
-            auth: proxyOpts.auth ?
-                { username: proxyOpts.auth.split(":")[0], password: proxyOpts.auth.split(":")[1] }
-                : undefined,
+            auth: proxyAuth(proxyOpts),
         };
         (config.proxy as any).protocol = proxyOpts.protocol;
     }
     return config;
+}
+
+function proxyAuth(proxyOpts: UrlWithStringQuery) {
+    if (proxyOpts.auth) {
+        const parts = proxyOpts.auth.split(":");
+        if (parts.length === 2) {
+            return {
+                username: parts[0],
+                password: parts[1],
+            }
+        }
+        throw new Error("Malformed Proxy authentication");
+    }
 }
