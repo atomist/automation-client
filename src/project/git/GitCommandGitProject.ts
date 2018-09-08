d6882ea08e268a6af53b47562137c82203d15c5c
Christian Dupuis <cd@atomist.com>
2018-09-08 13:13:34 +0200

    Clone wthouth tags

--- src/project/git/GitCommandGitProject.ts
+++ src/project/git/GitCommandGitProject.ts
@@ -1,6 +1,5 @@
 import { exec } from "child-process-promise";
 import * as _ from "lodash";
-import promiseRetry = require("promise-retry");
 import {
     ActionResult,
     successOn,
@@ -37,6 +36,7 @@ import {
     GitStatus,
     runStatusIn,
 } from "./gitStatus";
+import promiseRetry = require("promise-retry");
 
 export const DefaultDirectoryManager: DirectoryManager = TmpDirectoryManager;
 
