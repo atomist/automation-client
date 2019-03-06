a2ecdba1c789143cd77b51d252ccf2b51eba29cf
David Dooling <david@atomist.com>
2019-03-06 11:26:57 -0600

    Disable TSLint on generated types
    Add tslint:disable comment to top of generated types file.  Make
    things more async.  Only patch GraphQL TypeScript template if
    generating types.
    
    Closes #481

--- bin/gql-gen.ts
+++ bin/gql-gen.ts
@@ -20,12 +20,12 @@ import * as util from "util";
  * @param cwd directory to use as base for location of lib dir
  * @return Resolved, full path to lib directory
  */
-function libDir(cwd: string): string {
+async function libDir(cwd: string): Promise<string> {
     const lib = path.resolve(cwd, "lib");
     const src = path.resolve(cwd, "src");
-    if (fs.existsSync(lib)) {
+    if (await fs.pathExists(lib)) {
         return lib;
-    } else if (fs.existsSync(src)) {
+    } else if (await fs.pathExists(src)) {
         return src;
     } else {
         return lib;
@@ -43,11 +43,8 @@ async function patchGraphQLCodeGenerator(cwd: string): Promise<void> {
         // patch up codegen template to create optional properties
         const codegenTemplate = path.join(cwd, "node_modules", "graphql-codegen-typescript-client", "dist", "index.js");
         if (await fs.pathExists(codegenTemplate)) {
-            let contents = (await fs.readFile(codegenTemplate)).toString();
-            contents = contents.replace(
-                /{{ name }}: {{ convertedFieldType/,
-                "{{ name }}?: {{ convertedFieldType");
-            await fs.writeFile(codegenTemplate, contents);
+            const contents = await fs.readFile(codegenTemplate, "utf8");
+            await fs.writeFile(codegenTemplate, contents.replace(/{{ name }}: {{ convertedFieldType/, "{{ name }}?: {{ convertedFieldType"));
         }
     } catch (e) {
         console.error(`Failed to patch graphql-code-generator: ${e.message}`);
@@ -61,15 +58,13 @@ async function patchGraphQLCodeGenerator(cwd: string): Promise<void> {
 async function main(): Promise<void> {
     try {
         const cwd = process.cwd();
-        const lib = libDir(cwd);
-
-        await patchGraphQLCodeGenerator(cwd);
+        const lib = await libDir(cwd);
 
         // check if the project has a custom schema
         const customSchemaLocation = path.join(lib, "graphql", "schema.json");
         const defaultSchemaLocation = path.join(cwd, "node_modules", "@atomist", "automation-client", "lib",
             "graph", "schema.json");
-        const schema = fs.existsSync(customSchemaLocation) ? customSchemaLocation : defaultSchemaLocation;
+        const schema = (await fs.pathExists(customSchemaLocation)) ? customSchemaLocation : defaultSchemaLocation;
 
         const gqlGenOutput = path.join(lib, "typings", "types.ts");
         await fs.ensureDir(path.dirname(gqlGenOutput));
@@ -78,6 +73,7 @@ async function main(): Promise<void> {
 
         const config: Types.Config = {
             overwrite: true,
+            watch: false,
             schema: [schema],
             generates: {
                 [gqlGenOutput]: {
@@ -97,16 +93,21 @@ async function main(): Promise<void> {
         const graphqlFiles = await util.promisify(glob)(graphQlGlob);
 
         if (graphqlFiles && graphqlFiles.length > 0) {
+            await patchGraphQLCodeGenerator(cwd);
             config.documents = [graphQlGlob];
             await generate(config);
+            const typesContent = await fs.readFile(gqlGenOutput, "utf8");
+            await fs.writeFile(gqlGenOutput, `/* tslint:disable */\n\n${typesContent}`, "utf8");
         } else {
-            console.info("No GraphQL files found in project. Skipping type generation...");
+            console.info("No GraphQL files found in project, skipping type generation.");
         }
+        process.exit(0);
 
     } catch (e) {
         console.error(`Generating GraphQL types failed: ${e.message}`);
         process.exit(1);
     }
+    throw new Error("Should never get here, process.exit() called above");
 }
 
 main()
