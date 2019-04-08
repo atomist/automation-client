4bb5540982670f94316ef9491cb1c630892cf467
Christian Dupuis <cd@atomist.com>
2019-04-08 12:47:53 +0200

    Maintain internal prio queue of command/event invocations

--- bin/start.ts
+++ bin/start.ts
@@ -6,38 +6,38 @@
  */
 
 import "source-map-support/register";
-import { automationClient } from "../lib/automationClient";
-import { loadConfiguration } from "../lib/configuration";
-import { enableDefaultScanning } from "../lib/scan";
-import {
-    ClientLogging,
-    configureLogging,
-} from "../lib/util/logger";
 
-try {
-    configureLogging(ClientLogging);
-    loadConfiguration()
-        .then(configuration => {
-            enableDefaultScanning(configuration);
-            return configuration;
-        })
-        .then(configuration => automationClient(configuration).run())
-        .catch(e => {
-            if (e instanceof Error) {
-                if (e.stack && e.stack.includes(e.message)) {
-                    console.error(e.stack);
-                } else if (e.stack) {
-                    console.error(e.message);
-                    console.error(e.stack);
-                } else {
-                    console.error(e.message);
-                }
-            } else {
-                console.error(e);
-            }
-            process.exit(1);
-        });
-} catch (e) {
-    console.error(`Uncaught exception: ${e.message}`);
-    process.exit(10);
+function printError(e: any) {
+    if (e instanceof Error) {
+        if (e.stack && e.stack.includes(e.message)) {
+            console.error(e.stack);
+        } else if (e.stack) {
+            console.error(e.message);
+            console.error(e.stack);
+        } else {
+            console.error(e.message);
+        }
+    } else {
+        console.error(e);
+    }
 }
+
+async function main(): Promise<void> {
+    try {
+        const logging = require("../lib/util/logger");
+        logging.configureLogging(logging.ClientLogging);
+
+        let configuration = await require("../lib/configuration").loadConfiguration();
+        configuration = require("../lib/scan").enableDefaultScanning(configuration);
+        await require("../lib/automationClient").automationClient(configuration).run();
+    } catch (e) {
+        printError(e);
+        process.exit(5);
+    }
+}
+
+main()
+    .catch(e => {
+        console.error(`Unhandled exception: ${e.message}`);
+        process.exit(10);
+    });
