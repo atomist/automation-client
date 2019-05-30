6aba115d936545a3313724ff0df63e11e8d08dcd
James Carnegie <kipz@users.noreply.github.com>
2019-05-30 22:05:38 +0100

    Remove `atomist://github/default_repo_visibility` mapped parameter

--- lib/decorators.ts
+++ lib/decorators.ts
@@ -115,6 +115,9 @@ export abstract class MappedParameters {
     public static readonly GitHubWebHookUrl: string = "atomist://github_webhook_url";
     public static readonly GitHubUrl: string = "atomist://github_url";
     public static readonly GitHubApiUrl: string = "atomist://github_api_url";
+    /*
+    * @deprecated no alternative required
+    */
     public static readonly GitHubDefaultRepositoryVisibility: string = "atomist://github/default_repo_visibility";
     public static readonly GitHubUserLogin: string = "atomist://github/username";
 
