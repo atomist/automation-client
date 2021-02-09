fbd64731ac81294e624b201eff2894543ecdf94b
David Dooling <david@atomist.com>
2021-02-09 14:43:32 -0600

    Speed up test

--- test/project/support/AbstractProject.test.ts
+++ test/project/support/AbstractProject.test.ts
@@ -1,26 +1,11 @@
 import * as assert from "assert";
 import { GitHubRepoRef } from "../../../lib/operations/common/GitHubRepoRef";
-import {
-    AllFiles,
-    DefaultExcludes,
-    DefaultFiles,
-} from "../../../lib/project/fileGlobs";
 import { GitCommandGitProject } from "../../../lib/project/git/GitCommandGitProject";
-import { globMatchesWithin } from "../../../lib/project/support/AbstractProject";
 import { spawnPromise } from "../../../lib/util/child_process";
 
 describe("AbstractProject", () => {
 
     describe("globMatchesWithin", () => {
-
-        before(() => {
-            DefaultFiles.splice(1, DefaultFiles.length);
-        });
-
-        after(() => {
-            DefaultFiles.splice(0, DefaultFiles.length);
-            DefaultFiles.push(AllFiles, ...DefaultExcludes);
-        });
 
         it("should match ts pattern in nested folder", async () => {
             const patterns = [
@@ -32,7 +17,7 @@ describe("AbstractProject", () => {
 
             const p = await GitCommandGitProject.cloned(undefined, GitHubRepoRef.from({
                 owner: "atomist",
-                repo: "sdm-pack-build",
+                repo: "yaml-updater",
             } as any));
 
             await spawnPromise("npm", ["ci"], { cwd: p.baseDir });
@@ -42,7 +27,7 @@ describe("AbstractProject", () => {
             assert(matches.length > 1);
             assert(!matches.some(m => m.path.startsWith("node_modules")));
 
-        }).timeout(60000);
+        }).timeout(20000);
 
     });
 
